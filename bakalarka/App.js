b3012b818c2832fabc5392426680dca42dabdc1b
plosnak2 <jakub.zaukolec@gmail.com>
2022-02-13 10:08:36 +0100

    vytvorenie preddefinovanej spravy

--- bakalarka/App.js
+++ bakalarka/App.js
@@ -11,6 +11,7 @@ import ChatTrainer from './trainer/ChatTrainer';
 import { Ionicons } from '@expo/vector-icons';
 import {View} from "react-native";
 import PredefinedTrainer from './trainer/PredefinedTrainer';
+import AddPredefined from './trainer/AddPredefined';
 
 const Stack = createNativeStackNavigator();
 LogBox.ignoreLogs(['Setting a timer for a long period of time'])
@@ -61,14 +62,25 @@ export default function App() {
             <Ionicons name='calendar-outline' size={35} style={{marginLeft: 10}}/>
             <Ionicons name='images-outline' size={35} style={{marginLeft: 15}}/>
          </View>
-         })}/>
-         <Stack.Screen name="PredefinedTrainer" component={PredefinedTrainer} options={{
+        })}/>
+        <Stack.Screen name="PredefinedTrainer" component={PredefinedTrainer} options={({navigation})=> ({
           title: 'Preddefinované správy',
           headerStyle: {
            backgroundColor: '#c4c4c4'
          },
          headerTitleAlign: 'center',
-       }}/>
+         headerRight: () => 
+         <View style={{flexDirection: 'row', paddingRight:0}}>
+            <Ionicons name='add' size={35} style={{marginLeft: 10}} onPress={() => navigation.navigate('AddPredefined')}/>
+         </View>
+        })}/>
+        <Stack.Screen name="AddPredefined" component={AddPredefined} options={{
+          title: 'Vytvoriť novú správu',
+          headerStyle: {
+           backgroundColor: '#c4c4c4'
+         },
+         headerTitleAlign: 'center',
+        }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ bakalarka/trainer/AddPredefined.js
@@ -0,0 +1,63 @@
+import { ScrollView, Text, TouchableOpacity, View, StyleSheet, Image, TextInput, Keyboard, KeyboardAvoidingView } from "react-native";
+import React, { useEffect, useState } from 'react'
+import NavbarTrainer from "./Navbar";
+import { PredefinedRef } from "../firebasecfg";
+
+
+const AddPredefined = ({ navigation }) => {
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+
+    function saveMessage(){
+        PredefinedRef.add({
+            message: message,
+            name: name,
+            usage: 0
+        })
+        .then((docRef) => {
+            navigation.navigate('PredefinedTrainer')
+        })
+    }
+
+    return(
+    <View style={{flex:1}}>
+    <TouchableOpacity style={{flex:1, backgroundColor:"white"}} onPress={() => Keyboard.dismiss()}>
+        <View style={{alignItems:"center", marginTop:20}}>
+        <Text style={{fontWeight:"bold", fontSize:20}}>Zadajte názov:</Text>
+        <TextInput  multiline style={styles.input} onChangeText={newText => setName(newText)} value={name}/>
+        <Text style={{fontWeight:"bold", fontSize:20, marginTop:5}}>Zadajte správu:</Text>
+        <TextInput  multiline style={styles.input} onChangeText={newText => setMessage(newText)} value={message}/>
+        <TouchableOpacity style={styles.button2} onPress={() => saveMessage()}>
+            <Text style={{fontSize:20, fontWeight:"500"}}>Uložiť</Text>
+        </TouchableOpacity>
+        </View>
+    </TouchableOpacity>
+    
+    </View>
+    )
+};
+
+export default AddPredefined
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        marginBottom:70,
+    },
+    button2:{
+        width:"80%",
+        backgroundColor:"#c4c4c4",
+        height:50,
+        marginTop:30,
+        borderRadius:50,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    input:{
+        width:"80%",
+        borderWidth:1,
+        padding:10,
+        marginTop:10
+    },  
+})
